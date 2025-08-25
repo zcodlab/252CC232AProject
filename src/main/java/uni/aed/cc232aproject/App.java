f8690c5e9c1b160d8cd50758d95c804524720400
zcodlab <zmamanir@uni.edu.pe>
2025-08-25 09:24:05 -0500

    add ini project

---
+++ src/main/java/uni/aed/cc232aproject/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package uni.aed.cc232aproject;
+
+/**
+ *
+ * @author zemr
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
