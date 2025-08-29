9d98c22bbebcaa0c3a5772412e7488261a9a6f03
zcodlab <zmamanir@uni.edu.pe>
2025-08-29 10:29:22 -0500

    add maxmin package

---
+++ src/main/java/uni/aed/maxmin/MaxMin.java
@@ -0,0 +1,29 @@
+package uni.aed.maxmin;
+/**
+ *
+ * @author zemr
+ */
+public class MaxMin {
+    
+    public Integer getMaxValor(Integer[] X){
+        if(X ==null || X.length==0)
+            return -1;
+        Integer max=X[0];
+        for(int i=1;i<X.length;i++){
+            if(X[i]>max)
+                max=X[i];            
+        }
+        return max;
+    }
+
+    public Integer getMinValor(Integer[] X){
+        if(X ==null || X.length==0)
+            return -1;
+        Integer min=X[0];
+        for(int i=1;i<X.length;i++){
+            if(X[i]<min)
+                min=X[i];            
+        }
+        return min;
+    }
+}
