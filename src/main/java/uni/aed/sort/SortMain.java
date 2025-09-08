aa9d043a323e64d655025ffbb20d0301f52177f0
zmamanir@uni.edu.pe <>
2025-09-08 08:38:11 -0500

    add Cap11.E7

--- src/main/java/uni/aed/sort/SortMain.java
+++ src/main/java/uni/aed/sort/SortMain.java
@@ -1,5 +1,6 @@
 package uni.aed.sort;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class SortMain {
@@ -19,20 +20,22 @@ public class SortMain {
                 System.out.print("Algoritmos Ordenamiento"+SEPARADOR+
                         "1.- Carga de Datos en Array "+SEPARADOR+
                         "2.- Carga de Datos Duplicados en Array "+SEPARADOR+
-                        "3.- Visualizar Array "+SEPARADOR+
-                        "4.- Ordenar - metodo de Seleccion "+SEPARADOR+
-                        "5.- Salir "+SEPARADOR+
+                        "3.- Carga Aleatoria en Array "+SEPARADOR+
+                        "4.- Visualizar Array "+SEPARADOR+
+                        "5.- Ordenar - metodo de Seleccion "+SEPARADOR+
+                        "6.- Salir "+SEPARADOR+
                         "Elija una opcion:");
                 opcion=scr.nextInt();
                 switch(opcion)
                 {
                     case 1->{carga();visualizar();}
                     case 2->{cargaDuplicados();visualizar();}
-                    case 3->{visualizar();}
-                    case 4->{testSelectionSort();}
+                    case 3->{cargaAleatoria();visualizar();}
+                    case 4->{visualizar();}
+                    case 5->{testSelectionSort();}
                     default-> {break;}                
                }
-            }while(opcion!=5);
+            }while(opcion!=6);
             scr.close();
         }catch(Exception e){
             System.out.println("Error presentado "+ e.getMessage());
@@ -45,6 +48,18 @@ public class SortMain {
     }
     private void cargaDuplicados(){
         Integer X[]={5,14,24,14,43,5,84,45};        
+        dataOrigen=X.clone();
+        sort.setX(X);
+    }
+    private void cargaAleatoria(){       
+        System.out.println("Carga Aleatoria");
+        int N=5000;
+        int min=0;
+        int max=20000;
+        Integer[] X=new Integer[N];
+        Random random=new Random();
+        for(int i=0;i<X.length;i++)
+            X[i]=random.nextInt(max);
         dataOrigen=X.clone();
         sort.setX(X);
     }
