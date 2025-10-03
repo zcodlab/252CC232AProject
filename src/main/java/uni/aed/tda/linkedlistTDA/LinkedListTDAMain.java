d2a51e03eeebd3e4456981bb6b5b3a55883e6948
zmamanir@uni.edu.pe <>
2025-10-03 09:30:15 -0500

    add testLinkedListTDAPersona method

--- src/main/java/uni/aed/tda/linkedlistTDA/LinkedListTDAMain.java
+++ src/main/java/uni/aed/tda/linkedlistTDA/LinkedListTDAMain.java
@@ -1,12 +1,13 @@
 package uni.aed.tda.linkedlistTDA;
 
+import uni.aed.model.Persona;
 import uni.aed.tda.listTDA.ListTDA;
 
 public class LinkedListTDAMain {
     public static void main(String[] args){
         LinkedListTDAMain listTDAMain=new LinkedListTDAMain();
-        listTDAMain.testLinkedListTDAString();
-        //listTDAMain.testLinkedListTDAPersona();
+        //listTDAMain.testLinkedListTDAString();
+        listTDAMain.testLinkedListTDAPersona();
         
     }
     
@@ -43,4 +44,39 @@ public class LinkedListTDAMain {
         System.out.println(lista.toString());
     }
     
+    private void testLinkedListTDAPersona(){
+        ListTDA<Persona> lista=new LinkedListTDA<>();
+        System.out.println("Antes de añadir elementos");
+        System.out.println("La lista contiene "+lista.size()+" elementos");
+        Persona p1=new Persona("Sonya Smith",30,'F');
+        Persona p2=new Persona("Barack Obama",60,'M');
+        Persona p3=new Persona("Dina Boluarte",50,'F');
+        Persona p4=new Persona("Donald Trump",70,'M');
+        lista.add(p1);
+        lista.add(p2);
+        lista.add(p3);
+        System.out.println("Despues de añadir elementos");
+        System.out.println("La lista contiene "+lista.size()+" elementos");
+        
+        System.out.println("Visualizando la lista");
+        System.out.println(lista.toString());
+        
+        lista.add(2,p4);        
+        
+        System.out.println("Visualizando la lista");
+        System.out.println(lista.toString());        
+        
+        if(lista.contain(p4)){
+            System.out.println("El elemento buscado "+ p4.toString()+ 
+            " se encuentra en la posicion "+ lista.indexOf(p4)+
+                    ", se procedera a su eliminacion");
+            System.out.println(lista.delete(lista.indexOf(p4)));
+        }else
+            System.out.println("El elemento buscado "+ p4.toString()+ 
+            " NO se encuentra en la lista ");
+        
+        System.out.println("Visualizando la lista");
+        System.out.println(lista.toString());
+        
+    }
 }
