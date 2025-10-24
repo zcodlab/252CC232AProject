85d6d6733722eef4c2df43ced5cb2215d680d84f
zmamanir@uni.edu.pe <>
2025-10-24 10:40:47 -0500

    add LinkedQueueTDAMain class

--- src/main/java/uni/aed/tda/queueTDA/LinkedQueueTDAMain.java
+++ src/main/java/uni/aed/tda/queueTDA/LinkedQueueTDAMain.java
@@ -2,82 +2,84 @@ package uni.aed.tda.queueTDA;
 
 import uni.aed.model.Persona;
 
-public class ArrayQueueTDAMain {
-    public static void main(String[] args){
-        ArrayQueueTDAMain queueTDAMain=new ArrayQueueTDAMain(); 
-        queueTDAMain.testArrayQueueTDAInteger();
+public class LinkedQueueTDAMain {
+     public static void main(String[] args){
+        LinkedQueueTDAMain queueTDAMain=new LinkedQueueTDAMain(); 
+        queueTDAMain.testLinkedQueueTDAInteger();
         System.out.println("-----------------------------");
-        queueTDAMain.testArrayQueueTDAString();
+        queueTDAMain.testLinkedQueueTDAString();
         System.out.println("-----------------------------");
-        queueTDAMain.testArrayQueueTDAPersona();
+        queueTDAMain.testLinkedQueueTDAPersona();
     }
-    
-    private void testArrayQueueTDAInteger(){
-        QueueTDA<Integer> cola=new ArrayQueueTDA<>(4);
+     
+    private void testLinkedQueueTDAInteger(){
+        QueueTDA<Integer> cola=new LinkedQueueTDA<>();
         System.out.println("Antes de añadir elementos");
         System.out.println("La Cola contiene "+cola.size()+" elementos");
         cola.enqueue(30);
         cola.enqueue(10);
         cola.enqueue(20);
-        cola.enqueue(5);        
+        cola.enqueue(5);
         System.out.println("Despues de añadir elementos");
         System.out.println("La Cola contiene "+cola.size()+" elementos");
-        System.out.println("Visualizando la Cola");        
+        System.out.println("Visualizando la Cola");
         System.out.println(cola.toString());
-        
+
         System.out.println("Eliminando Elementos");
         System.out.println("Elemento Eliminado: "+cola.dequeue().toString());
-        System.out.println("Visualizando la Cola");        
+        System.out.println("Visualizando la Cola");
         System.out.println(cola.toString());
-        
+
         System.out.println("Recuperando Elementos");
         System.out.println("Elemento Recuperado: "+cola.peek().toString());
-        System.out.println("Visualizando la Cola");        
+        System.out.println("Visualizando la Cola");
         System.out.println(cola.toString());
     }
-    private void testArrayQueueTDAString(){
-        QueueTDA<String> cola=new ArrayQueueTDA<>(4);
+    private void testLinkedQueueTDAString(){
+        QueueTDA<String> cola=new LinkedQueueTDA<>();
         System.out.println("Antes de añadir elementos");
         System.out.println("La Cola contiene "+cola.size()+" elementos");
         cola.enqueue("Jose");
         cola.enqueue("Beatriz");
         cola.enqueue("Sandro");
-        cola.enqueue("Franklin");   
+        cola.enqueue("Franklin");
         System.out.println("Despues de añadir elementos");
         System.out.println("La Cola contiene "+cola.size()+" elementos");
-        
-        System.out.println("Visualizando la Cola");        
+
+        System.out.println("Visualizando la Cola");
         System.out.println(cola.toString());
         System.out.println("Eliminando Elementos");
         System.out.println("Elemento Eliminado: "+cola.dequeue());
-        System.out.println("Visualizando la Cola");        
+        System.out.println("Visualizando la Cola");
         System.out.println(cola.toString());
-        
+
         System.out.println("Recuperando Elementos");
         System.out.println("Elemento Recuperado: "+cola.peek());
-        System.out.println("Visualizando la Cola");        
+        System.out.println("Visualizando la Cola");
         System.out.println(cola.toString());
     }
-    private void testArrayQueueTDAPersona(){
-        QueueTDA<Persona> cola=new ArrayQueueTDA<>(4);
+    private void testLinkedQueueTDAPersona(){
+        QueueTDA<Persona> cola=new LinkedQueueTDA<>();
         cola.enqueue(new Persona("Jose",36,'M'));
         cola.enqueue(new Persona("Ana",46,'F'));
         cola.enqueue(new Persona("Juan",18,'M'));
         cola.enqueue(new Persona("Betty",66,'F'));
         System.out.println("Despues de añadir elementos");
         System.out.println("La Cola contiene "+cola.size()+" elementos");
-        
-        System.out.println("Visualizando la Cola");        
+
+        System.out.println("Visualizando la Cola");
         System.out.println(cola.toString());
         System.out.println("Eliminando Elementos");
         System.out.println("Elemento Eliminado: "+cola.dequeue());
-        System.out.println("Visualizando la Cola");        
+        System.out.println("Visualizando la Cola");
         System.out.println(cola.toString());
-        
+
         System.out.println("Recuperando Elementos");
         System.out.println("Elemento Recuperado: "+cola.peek());
-        System.out.println("Visualizando la Cola");        
+        System.out.println("Visualizando la Cola");
         System.out.println(cola.toString());
-        
+
     }
+
+    
 }
